886ceac1394277a1806a4674e14edd9d1625868a
TarunBapat <32245027+TarunBapat@users.noreply.github.com>
2021-11-11 19:22:18 +0530

    filter added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import { Card, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Card, Button, InputGroup, FormControl } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
 export default function App() {
   const [dataValue, setDataValue] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
   const fetchData = async () => {
     const response = await fetch(
       "https://jsonplaceholder.typicode.com/albums/1/photos"
@@ -13,9 +14,33 @@ export default function App() {
     setDataValue(data);
     console.log(data);
   };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const filterData = (e) => {
+    e.preventDefault();
+    setSearchValue(e.target.value);
+    console.log(e.target.value);
+  };
+  const searchData = () => {
+    const filteredData = dataValue.filter((val) =>
+      val.title.includes(searchValue)
+    );
+    setDataValue(filteredData);
+  };
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Enter value to be searched"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+          value={searchValue}
+          onChange={filterData}
+        />
+        <Button onClick={searchData}>Search</Button>
+      </InputGroup>
       <div className="d-flex justify-content-between align-items-center flex-column">
         {dataValue.map(({ id, title, url }) => {
           return (
