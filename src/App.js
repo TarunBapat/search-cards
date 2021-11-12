0a0f667e4e49e783e9ace19bf8129097c3fb08eb
TarunBapat <32245027+TarunBapat@users.noreply.github.com>
2021-11-12 17:06:03 +0530

    searchFilter added onchange for every keystroke

--- src/App.js
+++ src/App.js
@@ -17,45 +17,53 @@ export default function App() {
   useEffect(() => {
     fetchData();
   }, []);
-  const filterData = (e) => {
-    e.preventDefault();
-    setSearchValue(e.target.value);
-    console.log(e.target.value);
-  };
-  const searchData = () => {
-    const filteredData = dataValue.filter((val) =>
-      val.title.includes(searchValue)
-    );
-    setDataValue(filteredData);
-  };
+
+  // const searchData = () => {
+  //   const filteredData = dataValue.filter((val) =>
+  //     val.title.includes(searchValue)
+  //   );
+  //   setDataValue(filteredData);
+  // };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Search Cards</h1>
       <InputGroup className="mb-3">
         <FormControl
           placeholder="Enter value to be searched"
           aria-label="Recipient's username"
           aria-describedby="basic-addon2"
           value={searchValue}
-          onChange={filterData}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+          }}
         />
-        <Button onClick={searchData}>Search</Button>
+        {/* <Button onClick={searchData}>Search</Button> */}
       </InputGroup>
       <div className="d-flex justify-content-between align-items-center flex-column">
-        {dataValue.map(({ id, title, url }) => {
-          return (
-            <>
-              <Card style={{ width: "18rem", marginBottom: "2rem" }}>
-                <Card.Img variant="top" src={url} />
-                <Card.Body>
-                  <Card.Title>{title}</Card.Title>
+        {dataValue
+          .filter((val) => {
+            if (searchValue === "") {
+              return val;
+            } else if (
+              val.title.toLowerCase().includes(searchValue.toLowerCase())
+            ) {
+              return val;
+            }
+          })
+          .map(({ id, title, url }) => {
+            return (
+              <>
+                <Card style={{ width: "18rem", marginBottom: "2rem" }}>
+                  <Card.Img variant="top" src={url} />
+                  <Card.Body>
+                    <Card.Title>{title}</Card.Title>
 
-                  <Button variant="success">checkout card {id}</Button>
-                </Card.Body>
-              </Card>
-            </>
-          );
-        })}
+                    <Button variant="success">checkout card {id}</Button>
+                  </Card.Body>
+                </Card>
+              </>
+            );
+          })}
       </div>
 
       <button onClick={fetchData}>fetch Data</button>
